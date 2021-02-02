1790aaa82b5c0da13e21cbc805dd0979a72bf0cf
chakravarthi <68289097+chakravarthi-giduthuri@users.noreply.github.com>
2021-02-02 22:50:18 +0530

    add of footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Copyright &copy; 2020 <strong>maddcoder</strong>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
